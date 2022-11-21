5fef47c1a227de52c54891b8e3d3247c9fe7e966
timothykiyeng <timothy.kiyeng@student.moringaschool.com>
2022-11-21 14:53:26 +0300

    Done

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,5 +4,9 @@ class StudentsController < ApplicationController
     students = Student.all
     render json: students
   end
+  def first_student
+    student = Student.find_by(id: params[:id])
+    render json:student
+  end
 
 end
